e7ac5ceb322343fbcb42d79d06b198c9cf023ee7
Murilo Lobato <muilo_lobato@outlook.com>
2020-08-01 14:44:00 -0300

    Non-relational Student's Domain Rules

--- SCM/Scm.Domain/Student.cs
+++ SCM/Scm.Domain/Student.cs
@@ -1,4 +1,5 @@
-﻿using Scm.Infra.CrossCutting.Enum;
+﻿using Scm.Infra.CrossCutting.CustomException;
+using Scm.Infra.CrossCutting.Enum;
 using System;
 using System.Collections.Generic;
 
@@ -24,13 +25,37 @@ namespace Scm.Domain
         public string FirstName
         {
             get { return _firstName; }
-            private set { _firstName = value; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new DomainRulesException("A name is needed when creating a new Task");
+
+                if (value.Length > 30)
+                    throw new DomainRulesException("The Student's name can not be longer than 30 characteres");
+
+                if (value.Length < 3)
+                    throw new DomainRulesException("The Student's name can not be shorter than 3 characteres");
+                
+                _firstName = value;
+            }
         }
 
         public string Surname
         {
             get { return _surname; }
-            private set { _surname = value; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new DomainRulesException("A name is needed when creating a new Task");
+
+                if (value.Length > 120)
+                    throw new DomainRulesException("The Student's name can not be longer than 120 characteres");
+
+                if (value.Length < 3)
+                    throw new DomainRulesException("The Student's name can not be shorter than 3 characteres");
+               
+                _surname = value; 
+            }
         }
 
         public GenderEnum Gender { get; private set; }
@@ -38,7 +63,13 @@ namespace Scm.Domain
         public DateTime DateOfBirth
         {
             get { return _dateOfBirth; }
-            private set { _dateOfBirth = value; }
+            private set
+            {
+                if (value > DateTime.Now)
+                    throw new DomainRulesException("Date of birth can not be setted in the future");
+
+                _dateOfBirth = value; 
+            }
         }
 
         public ICollection<Address> Addresses { get; private set; } = new HashSet<Address>();
---
+++ SCM/Scm.Infra.CrossCutting/CustomException/DomainRulesException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Scm.Infra.CrossCutting.CustomException
+{
+    public class DomainRulesException : Exception
+    {
+        public DomainRulesException() { }
+
+        public DomainRulesException(string message) : base(message) { }
+
+        public DomainRulesException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
